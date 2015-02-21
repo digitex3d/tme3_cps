6469d945b07d11a5614ec790c6850cd5d031d186
digitex3d <digitex3d@gmail.com>
2015-02-21 12:09:38 +0100

    Precisazione nelle stringhe

--- src/filesprio/PostConditionError.java
+++ src/filesprio/PostConditionError.java
@@ -9,6 +9,6 @@ public class PostConditionError extends Exception {
 	private static final long serialVersionUID = 6464471104164196138L;
 	
 	public PostConditionError(String msg) {
-		super(msg);
+		super("PostCondition error: " + msg);
 	}
 }
